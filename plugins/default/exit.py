9199ad930a5321cbcac2c16497136fca6df31582
Harry P <harrypeach1999@gmail.com>
2018-02-26 15:49:10 +0000

    Finished exit plugin

--- plugins/default/exit.py
+++ plugins/default/exit.py
@@ -1,13 +1,27 @@
 """
     Default exit plugin
 """
+import shutil
+import logging
+import os
 
 
 class ExitPlugin(object):
-    def quit():
-        pass
+    """ Removes temporary files and exits the program """
+
+    def __init__(self, skye):
+        self.skye = skye
+
+    def close_program(self):
+        """ Closes the program """
+        self.skye.speak("Goodbye")
+        logging.debug("Removing temporary folders")
+        if os.path.exists("temp"):
+            shutil.rmtree("temp", ignore_errors=True)
+        logging.info("Exiting")
+        quit()
 
 
 def setup(skye):
     exit_plugin = ExitPlugin(skye)
-    skye.register_command('exit', quit)
+    skye.register_command('exit', exit_plugin.close_program)
