975a1af1c1ca1f6c48148d7dba3e688c64a3d52b
Harry P <harrypeach1999@gmail.com>
2018-02-27 22:00:32 +0000

    Updated onboarding to add a sample command

--- utils/onboarding.py
+++ utils/onboarding.py
@@ -8,5 +8,23 @@ class Onboarding(object):
 
     def onboard(self):
         """ Function to manage onboarding features """
-        print("Onboard function called")
+        wake_word = self.skye.config.get("general", "wake_word")
+        self.skye.speak("Hi, I'm Skye, your open virtual assistant.")
+        self.skye.speak("Let's set up a few things before we begin.")
+        self.skye.speak("Your current wake word is: " + wake_word)
+        self.skye.speak("Let's try a sample command.")
+        self.skye.speak(f"Say: {wake_word} hello")
+        success_flag = False
+        while not success_flag:
+            words = self.skye.active_listen()
+            if (words != -1 and
+                words != -2 and
+                    words != -3):
+                if words == f"{wake_word} hello":
+                    self.skye.speak("Good, the command was a success.")
+                    success_flag = True
+            else:
+                self.skye.speak("Sorry, I didn't understand you.")
+        self.skye.speak("Setup is successful.")
+        # Change config onboarding to false.
         pass
