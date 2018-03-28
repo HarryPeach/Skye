eddeb7f2786e00b3192225c2eaf81733b29cf26e
Harry Peach <harrypeach1999@gmail.com>
2018-03-28 22:56:58 +0100

    Finished system plugin

--- plugins/skye/system.py
+++ plugins/skye/system.py
@@ -2,6 +2,7 @@
     Default System management plugin
 """
 import logging
+import subprocess
 
 
 class SystemPlugin(object):
@@ -13,7 +14,57 @@ class SystemPlugin(object):
     def shutdown_system(self):
         """Shuts down the host system
         """
-        logging.debug("Rolling a dice")
+        if self.confirm_action("shut down"):
+            logging.debug("Performing shutdown action")
+            self.skye.speak("Shutting down system")
+            subprocess.call(["shutdown", "/s"])
+        else:
+            logging.debug("Shutdown action cancelled by user")
+            self.skye.speak("Action cancelled")
+
+    def logout_system(self):
+        """Logs out the host system
+        """
+        if self.confirm_action("log out"):
+            logging.debug("Performing logout action")
+            self.skye.speak("Logging out of system")
+            subprocess.call(["shutdown", "/l"])
+        else:
+            logging.debug("Logout action cancelled by user")
+            self.skye.speak("Action cancelled")
+
+    def restart_system(self):
+        """Restarts the host system
+        """
+        if self.confirm_action("restart"):
+            logging.debug("Performing restart action")
+            self.skye.speak("Restarting system")
+            subprocess.call(["shutdown", "/r"])
+        else:
+            logging.debug("Restart action cancelled by user")
+            self.skye.speak("Action cancelled")
+
+    def confirm_action(self, action):
+        """Asks a user for confirmation before performing
+        an action
+
+        Arguments:
+            action {string} -- The action to be performed
+
+        Returns:
+            boolean -- Whether the user confirmed the action or not
+        """
+        self.skye.speak(f"Are you sure you want to {action}?")
+        word = self.skye.active_listen()
+        if (word != -1 and
+            word != -2 and
+                word != -3):
+            if word == "yes":
+                return True
+            elif word == "no":
+                return False
+        else:
+            self.skye.speak("There was an error understanding you.")
 
 
 def setup(skye):
@@ -26,3 +77,7 @@ def setup(skye):
     system_plugin = SystemPlugin(skye)
     skye.register_command(("shutdown", "shut down", "turn off"),
                           system_plugin.shutdown_system)
+    skye.register_command(("logout", "log out"),
+                          system_plugin.logout_system)
+    skye.register_command(("restart", "reboot"),
+                          system_plugin.logout_system)
